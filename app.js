67a1878fdefa4ec21b4a5d54bb52851d900af582
Saqib-Alam <saqibalam64@gmail.com>
2024-12-05 03:03:16 +0500

    weather app

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 // const API_URL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
-
-const API_KEY = "92ec2805af9962140156fd754f8dc4e3"
+const API_KEY = "92ec2805af9962140156fd754f8dc4e3";
+const REVERSE_GEO_URL = "https://api.openweathermap.org/geo/1.0/reverse";
 const API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+let currentCity="";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon")
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const fetchCityName = async (latitude, longitude) => {
+        try {
+            const response = await fetch(
+                `${REVERSE_GEO_URL}?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`
+            );
+
+            const data = await response.json();
+                currentCity=data[0].name;
+                checkWeather(currentCity);
+
+            }
+         catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+                fetchCityName(latitude, longitude);
+            },
+            (error) => {
+                console.error("Geolocation Error:", error);
+            }
+        );
+    } else {
+         alert("Geolocation is not supported by your browser.");
+    }
+});
+
 
 async function checkWeather(city){
     const response = await fetch(API_URL + city + `&appid=${API_KEY}`);
@@ -58,24 +95,3 @@ searchBtn.addEventListener("click", ()=>{
 })
 
 
-// console.log(navigator.geolocation)
-
-// if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//             const { latitude, longitude } = position.coords;
-//             // fetchWeather(latitude, longitude);
-//         },
-//         (error) => {
-//             locationEl.textContent = "Unable to fetch location.";
-//             console.error("Geolocation error:", error);
-//         }
-//     );
-// } else {
-//     locationEl.textContent = "Geolocation is not supported by your browser.";
-// }
-
-
-// window.onload = function() {
-//     checkWeather("karachi");
-// };
